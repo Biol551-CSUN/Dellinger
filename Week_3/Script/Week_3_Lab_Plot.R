30097686eef772c933821da2e83f3f63141dfa38
Rob <robertdellinger0@gmail.com>
2022-02-15 14:39:03 -0800

    Commit

---
+++ Week_3/Script/Week_3_Lab_Plot.R
@@ -0,0 +1,37 @@
+---
+  title: "Week_3_Lab_HW"
+author: "Robert Dellinger"
+date: "2/10/2022"
+---
+  
+  
+#Install Packages
+install.packages("palmerpenguins")
+install.packages("tidyverse")
+install.packages("here")
+install.packages("devtools")
+devtools::install_github("dill/beyonce")
+
+#Load Libraries
+library(palmerpenguins)
+library(tidyverse)
+library(here)
+
+#Data Visualization
+glimpse(penguins)
+penguins<-na.omit(penguins)
+
+ggplot(data=penguins, 
+       mapping=aes(x=species,
+                   y=body_mass_g,
+                   fill=sex)) + #sex
+  geom_boxplot(outlier.shape = NA)+
+  labs(title = "Body Mass & Biological Sex of Penguins",
+       subtitle = "Differences for Adelie, Chinstrap, and Gentoo Penguins",
+       x = "Species", y = "Body Mass (g)",
+       fill = "Sex",
+       caption = "Source: Palmer Station LTER / palmerpenguins package")+
+  theme_bw()+
+  theme(axis.title = element_text(size = 20),
+        panel.background = element_rect(fill = "white")) 
+  

3989b7015c984ae859aa1277bbb2f907dc0f0c2b
Rob <robertdellinger0@gmail.com>
2022-02-15 16:06:36 -0800

    Plot

---
+++ Week_4/Script/Week_4_Lab_Plot.R
@@ -0,0 +1,69 @@
+
+### Lab: Dyplr ##############
+### Created by: Robert Delllinger
+### Date: 02/15/2022 ############
+
+
+## Laod Libraries ###############
+
+library(palmerpenguins)
+library(tidyverse)
+library(here) 
+library(beyonce)
+library(devtools)
+devtools::install_github("dill/beyonce")
+
+## Load Data ####################
+glimpse(penguins)
+head(penguins)
+
+## Part 1 ####################
+penguins %>%
+  drop_na(species, island, sex) %>% 
+  group_by(species, island, sex) %>% 
+  summarize(mean_body_mass_g = mean(body_mass_g, na.rm = TRUE), 
+            variance_body_mass_g = var(body_mass_g, na.rm = TRUE))
+                      
+## Part 2 ####################
+
+plot <-penguins %>% # use penguin data frame
+  filter(sex != "male") %>% #filter out males
+  mutate(log_mass = log(body_mass_g)) %>% #calculate log biomass
+  select(species, island, sex, log_mass) %>% # select species, island, sex, and log biomasss
+  ggplot(aes(x = species, y = log_mass, fill = species)) + # plot log biomass per species of female penguins 
+  geom_boxplot(outlier.shape = NA) +
+  labs(title = "Log Body Mass of Female Penguins", #creating labels
+       subtitle = "Differences for Adelie, Chinstrap, and Gentoo Penguins",
+       x = "Species", y = "Log Body Mass (g)", fill = "Species",caption = "Source: Palmer Station LTER / palmerpenguins package")+
+     scale_fill_manual(values= beyonce_palette(18))+
+  theme_linedraw()+
+  theme(title = element_text(size = 15), axis.title = element_text(size = 15),
+        panel.background = element_rect(fill = "lightcyan1")) 
+  
+plot 
+
+ggsave(here('Week_4/Output', "Box_Plot_Figure.png"), plot) #save plot to an output folder
+  
+  
+
+ggplot(data=penguins, 
+               mapping=aes(x=species,
+                           y=body_mass_g,
+                           fill=sex)) + #sex
+  geom_boxplot(outlier.shape = NA)+
+  labs(title = "Body Mass & Biological Sex of Penguins",
+       subtitle = "Differences for Adelie, Chinstrap, and Gentoo Penguins",
+       x = "Species", y = "Body Mass (g)",
+       fill = "Sex",
+       caption = "Source: Palmer Station LTER / palmerpenguins package")+
+  theme_bw()+
+  theme(axis.title = element_text(size = 20),
+        panel.background = element_rect(fill = "white")) 
+
+
+
+
+  group_by(island) %>% #summarizing penguin data by island (must be placed before summarize) 
+  summarize(mean_bill_length = mean(bill_length_mm, na.rm = TRUE),
+            max_bill_length = max(bill_length_mm, na.rm=TRUE))
+  
